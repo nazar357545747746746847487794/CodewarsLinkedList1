3dde8b7a799c07d43671303ae183551cea86d269
Nazar <107304468+nazar357545747746746847487794@users.noreply.github.com>
2026-03-27 21:26:10 +0200

    Add files via upload

---
+++ Linked Lists - Push & BuildOneTwoThree/solutione 2.py
@@ -0,0 +1,19 @@
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def push(head, data):
+    node = Node(data)
+    node.next = head
+    return node
+
+def build_one_two_three():
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
